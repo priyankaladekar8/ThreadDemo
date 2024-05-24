a1a015e2c5b061f37c8668c5ef754004d7edef6c
priyankaladekar8 <ladekarpriyanka95@gmail.com>
2024-05-24 15:40:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadDemo
+{
+    public class Demo
+    {
+        public void print1()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public void print2()
+        {
+            for (int i = 11; i <= 20; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo demo = new Demo();
+            Thread t1 = new Thread(new ThreadStart(demo.print1)); //deletegates 
+            Thread t2 = new Thread(new ThreadStart(demo.print2));
+            t1.Start();
+            t2.Start();
+
+        }
+    }
+}
